3c5d47b071d094eff0088b7897ca39b10dc46e89
NikitaHaroshka <nikiharoshka@gmail.com>
2024-10-16 15:49:30 +0300

    trying to multithread

--- lab3.cpp
+++ lab3.cpp
@@ -1,15 +1,56 @@
+#include <windows.h>
 #include <iostream>
-#include <Windows.h>
 using namespace std;
+HANDLE hSemaphore;
+volatile int kek = 0;
+
+DWORD WINAPI marker(LPVOID Param) {
+	int index = *(int*)Param;
+	WaitForSingleObject(hSemaphore, INFINITE);
+	//cout << endl << index;
+	//Sleep(100);
+	kek++;
+	cout << "Thread " << index << ": kek = " << kek << endl;
+
+	ReleaseSemaphore(hSemaphore, 1, NULL);
+	//cout << kek;
+	return 0;
+}
 
 int main() {
-	int sizeArr, numOfThread;
-	cin >> sizeArr;
-	int* A = new int[sizeArr];
-	for (int i = 0; i < sizeArr; i++) {
+	int k, numOfThreads;
+	cin >> k;
+	volatile int n=k;
+	volatile int* A = new int[n];
+	for (int i = 0; i < n; i++) {
 		A[i] = 0;
 	}
-	cin >> numOfThread;
+	cin >> numOfThreads;
+	HANDLE* hThread = new HANDLE[numOfThreads];
+	int* Indexs = new int[numOfThreads];
 
+	//Создание семафора
+	hSemaphore = CreateSemaphore(NULL, 1, 1, NULL);
+
+
+	//Создание потоков
+	for (int i = 0; i < numOfThreads; i++) {
+		Indexs[i] = i;
+		hThread[i] = CreateThread(NULL,0,marker,(void*)&Indexs[i], 0, NULL);
+		if (hThread[i] == NULL) {
+			return GetLastError();
+			return 1;
+		}
+
+	}
+	
+	WaitForMultipleObjects(numOfThreads, hThread,TRUE,INFINITE);
+	CloseHandle(hSemaphore);
+	//Закрытие потоков
+	for (int i = 0; i < numOfThreads; i++) {
+		CloseHandle(hThread[i]);
+	}
+	delete[] A;
+	
 	return 0;
 }
